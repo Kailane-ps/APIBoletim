481210ae356a2576b655487f243f486ca42bb0aa
FIC <FIC@info.local>
2020-09-03 12:21:22 -0300

    APIBoletim Finalizado

---
+++ Domains/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletimTarde.Domains
+{
+    public class Aluno
+    {
+        public int IdAluno { get; set; }
+        public string Nome  { get; set; }
+        public string RA { get; set; }
+        public int Idade { get; set; }
+
+    }
+}
